d58701805e897d2ba725db9608bcaeb97b8f0aae
W Akr <mr.waleedakram@gmail.com>
2022-07-23 12:19:14 +0100

    fix: prettier eslint

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,17 @@ import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule, {
-    transport: Transport.REDIS,
-    options: {
-      url: "redis://127.0.0.1:6379",
-      retryAttempts: 2,
-      retryDelay: 500
-    }
-  });
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.REDIS,
+      options: {
+        url: 'redis://127.0.0.1:6379',
+        retryAttempts: 2,
+        retryDelay: 500,
+      },
+    },
+  );
   await app.listen();
 }
 bootstrap();
